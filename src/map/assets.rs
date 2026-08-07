3e0abaf350d10e4ea3a29edcbc92c75d75b09b33
Alec <alhampton9@gmail.com>
2026-08-07 11:47:42 -0600

    Progress on implementing procedural world generation

--- src/map/assets.rs
+++ src/map/assets.rs
@@ -30,6 +30,7 @@ impl SpawnableAsset {
     }
 }
 
+
 #[derive(Clone)]
 pub struct TilemapHandles {
     pub image: Handle<Image>,
@@ -43,4 +44,55 @@ impl TilemapHandles {
             TextureAtlas::from(self.layout.clone()).with_index(atlas_index),
         )
     }
+}
+
+
+pub fn prepare_tilemap_handles(
+    asset_server: Res<AssetServer>,
+    atlas_layouts: &mut ResMut<Assets<TextureAtlasLayout>>,
+    assets_directory: &str,
+    tilemap_file: &str,
+) -> TilemapHandles {
+    let image = asset_server.load::<Image>(format!("{assets_directory}/{tilemap_file}"));
+    let mut layout = TextureAtlasLayout::new_empty(TILEMAP.atlas_size());
+    for index in 0..TILEMAP.sprites.len() {
+        layout.add_texture(TILEMAP.sprite_rect(index));
+    }
+    let layout = atlas_layouts.add(layout);
+
+    TilemapHandles { image, layout }
+}
+
+pub fn load_assets(
+    tilemap_handles: &TilemapHandles,
+    asset_definitions: Vec<Vec<SpawnableAsset>>
+) -> ModelsAssets<Sprite> {
+    let mut model_assets = ModelAssets::<Sprite>::new();
+    // for each type of tile...
+    for (model_index, assets) in asset_definitions.into_iter().enumerate() {
+        // for each sprite that tile needs...
+        for asset_def in assets {
+            let SpawnableAsset {
+                sprite_name,
+                grid_offset,
+                offset,
+                components_spawner,
+            } = asset_def;
+
+            let Some(atlas_index) = TILEMAP.sprite_index(sprite_name) else {
+                panic!("Uknown atlas sprite '{}'", sprite_name);
+            };
+
+            model_assets.add(
+                model_index,
+                ModelAsset {
+                    assets_bundle: tilemap_handles.sprite(atlas_index),
+                    grid_offset,
+                    world_offset: offset,
+                    spawn_commands: components_spawner,
+                },
+            )
+        }
+    }
+    model_assets
 }
